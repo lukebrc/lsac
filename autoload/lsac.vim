95ae1c30b61efec782b9310beaf4c893f72eddd2
Lukasz Barcinski <lukaszb.gwz@gmail.com>
2019-04-10 01:15:21 +0200

    Functions for simple parsing python functions

--- autoload/lsac.vim
+++ autoload/lsac.vim
@@ -5,9 +5,16 @@ endif
 
 let s:pyscript = resolve('~/.vim/bundle/lsac/parse.py')
 
-function! Append()
+function! LsacComplete()
     let s:wordUnderCursor = expand("<cword>")
-    let s:currentLine   = getline(".")
+    execute 'python import sys'
+    execute 'python sys.argv = ["complete"]'
+    "let s:currentLine = getline(".")
     execute 'pyfile ' . s:pyscript
 endfunction
 
+function! LsacParse()
+    execute 'python import sys'
+    execute 'python sys.argv = ["parse"]'
+    execute 'pyfile ' . s:pyscript
+endfunction
--- parse.py
+++ parse.py
@@ -1,6 +1,45 @@
 import vim
 import re
+import sys
 
-print(len(vim.current.buffer))
-for line in vim.current.buffer:
-    print("line: " + line)
+
+def getClass(word):
+    for line in vim.current.buffer:
+        r1 = re.findall(r"%s\s*=\s*([a-zA-Z_]+)" % (currentWord), line)
+        if len(r1) > 0:
+            return r1[0]
+    return None
+
+
+def complete(word):
+    className = getClass(word)
+    if className is None:
+        return []
+    return objMap[className]
+
+
+def parseClasses(lines):
+    currentClass = None
+    for line in lines:
+        r1 = re.findall(r"\s*class\s+([a-zA-Z_]+)", line)
+        if len(r1) > 0:
+            currentClass = r1[0]
+            objMap[currentClass] = []
+        elif currentClass is not None:
+            r1 = re.findall(r"def\s+([a-zA-Z_]+)", line)
+            if len(r1) > 0:
+                objMap[currentClass].append(r1[0])
+
+
+try:
+    objMap
+except NameError:
+    objMap = {}
+    parseClasses(vim.current.buffer)
+
+if sys.argv[0] == 'parse':
+    parseClasses(vim.current.buffer)
+else:
+    currentWord = vim.eval('s:wordUnderCursor')
+    functions = complete(currentWord)
+    print(functions)
